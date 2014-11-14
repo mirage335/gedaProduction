c39aba2b781459e98c3777444727220ed87fb02d
mirage335 <spamfreemirage335@gmail.com>
2014-11-14 17:22:03 +0000

    CNC milling added to refresh script.

--- refreshAll.sh
+++ refreshAll.sh
@@ -13,13 +13,13 @@ find . -name genBOM.sh -type f -exec bash -c 'cd $(dirname $(realpath {})) ; ./g
 
 echo -e '\E[1;32;46m done. \E[0m\n'
 
-echo -en '\E[1;32;46m Regenerating oshpark specifications... \E[0m'
+echo -en '\E[1;32;46m Regenerating oshpark/oshstencil specifications... \E[0m'
 
 #Regerenate oshpark-compatible gerber packages.
 find . -name generate-gerbers.sh -type f -exec bash -c 'cd $(dirname $(realpath {})) ; ./generate-gerbers.sh' \; > /dev/null
 
 #Regerenate oshpark-compatible gerber packages.
-find . -name generate-stencils.sh -type f -exec bash -c 'cd $(dirname $(realpath {})) ; ./generate-gerbers.sh' \; > /dev/null
+find . -name generate-stencils.sh -type f -exec bash -c 'cd $(dirname $(realpath {})) ; ./generate-stencils.sh' \; > /dev/null
 
 echo -e '\E[1;32;46m done. \E[0m\n'
 
@@ -27,6 +27,13 @@ echo -en '\E[1;32;46m Regenerating CAD data... \E[0m'
 
 #Regerenate CAD data.
 find . -name generate-cad.sh -type f -exec bash -c 'cd $(dirname $(realpath {})) ; ./generate-cad.sh' \; >& /dev/null
+
+echo -e '\E[1;32;46m done. \E[0m\n'
+
+echo -en '\E[1;32;46m Regenerating CNC isolation milling assets, this may take a few minutes... \E[0m'
+
+#Regenerate isolation milling data.
+find . -name generate-cnc.sh -type f -exec bash -c 'cd $(dirname $(realpath {})) ; ./generate-cnc.sh' \; >& /dev/null
 
 echo -e '\E[1;32;46m done. \E[0m\n'
 
